f54688a9ee8d4c46dcc403dc7c4d103a2255a843
G4m3rGrl <thea.t.crona@gmail.com>
2025-04-09 14:10:51 +0200

    Lade till kommentarer

--- projektet/canvas.js
+++ projektet/canvas.js
@@ -4,11 +4,13 @@ function main() {
   const ctx = canvas.getContext("2d");
 
   //Konstanter för spelelement
+  //Spelaren
   const PLAYER_COLOR = "#000";
   const PLAYER_WIDTH = 100;
   const PLAYER_HEIGHT = 20;
   const MOVEMENT_SPEED = 20;
 
+  //Blocken
   const BLOCK_HEIGHT = 25;
   const BLOCK_COLOR = "#000";
   const BLOCK_GAP = 3;
@@ -16,15 +18,16 @@ function main() {
   const BLOCK2_AMOUNT = 5;
   const BLOCK3_AMOUNT = 3;
 
+  //Bollen
   const BALL_SIZE = 10;
   const BALL_COLOR = "#000";
   const BALL_REFRESH_RATE = 5;
 
-  //Sätter startkoordinater för spelaren.
-  var playerX = canvas.width * 0.5 - PLAYER_WIDTH / 2;
+  //Sätter startkoordinater för spelaren i mitten.
+  var playerX = canvas.width * 0.5 - PLAYER_WIDTH/2;
   var playerY = canvas.height * 0.8;
 
-  //Klass för block.
+  //Klass för block med 4 egenskaper.
   class Block {
     constructor(x, y, width, height) {
       this.x = x;
@@ -33,6 +36,7 @@ function main() {
       this.height = height;
     }
   }
+  //Variabel för block samt 3 arrayer där block läggs efter de skapats.
   var blockY = 100;
   var blockLayer1 = [];
   var blockLayer2 = [];
@@ -44,37 +48,23 @@ function main() {
   var ballMoveX = 1;
   var ballMoveY = 1;
 
-  //Funktion för initiering av variabler, så att de enkelt ska gå att justera.
-  function initBlock (layer, amount) {
-    for (i = 0; i < amount; i++) {
-      layer.push(true);
-    }
-  }
-
-  initBlock(blockLayer1, BLOCK1_AMOUNT);
-  initBlock(blockLayer2, BLOCK2_AMOUNT);
-  initBlock(blockLayer3, BLOCK3_AMOUNT);
-
-  //En funktion som ritar alla block från arrayerna.
-  function drawBlock(layer) {
+  //En funktion som ritar alla block och skapar instanser av block-klassen
+  //som sedan läggs till i arrayerna.
+  function drawBlock(layer, amount) {
     ctx.fillStyle = BLOCK_COLOR;
-    let blockWidth = (canvas.width - BLOCK_GAP) / layer.length - BLOCK_GAP;
+    let blockWidth = (canvas.width - BLOCK_GAP) / amount - BLOCK_GAP;
     let blockX = BLOCK_GAP;
-    for (i = 0; i < layer.length; i++) {
+    for (i = 0; i < amount; i++) {
       ctx.fillRect(blockX, blockY, blockWidth, BLOCK_HEIGHT);
       blockX += blockWidth + BLOCK_GAP;
-      layer[i] = new Block(blockX, blockY, blockWidth, BLOCK_HEIGHT);
+      layer.push(new Block(blockX, blockY, blockWidth, BLOCK_HEIGHT));
     }
     blockY += BLOCK_HEIGHT + BLOCK_GAP;
   }
   //Kör funktionerna för varje array
-  drawBlock(blockLayer1);
-  drawBlock(blockLayer2);
-  drawBlock(blockLayer3);
-
-  console.log(blockLayer1);
-  console.log(blockLayer2);
-  console.log(blockLayer3);
+  drawBlock(blockLayer1, BLOCK1_AMOUNT);
+  drawBlock(blockLayer2, BLOCK2_AMOUNT);
+  drawBlock(blockLayer3, BLOCK3_AMOUNT);
 
   //Ritar spelaren enligt angivna variabler & konstanter.
   function drawPlayer() {
@@ -83,14 +73,16 @@ function main() {
   }
   drawPlayer();
 
-  //Kör funktion om spelare trycker på tangent.
+  //Kör funktion om spelare trycker på tangent. Styr spelaren.
   window.onkeypress = function (e) {
     //Om trycker på z
     if (e.keyCode == 122) {
       ctx.clearRect(playerX, playerY, PLAYER_WIDTH, PLAYER_HEIGHT);
       playerX -= MOVEMENT_SPEED;
       drawPlayer();
-    } else if (e.keyCode == 120) {
+    }
+    //Om trycker på x
+    else if (e.keyCode == 120) {
       ctx.clearRect(playerX, playerY, PLAYER_WIDTH, PLAYER_HEIGHT);
       playerX += MOVEMENT_SPEED;
       drawPlayer();
@@ -98,16 +90,19 @@ function main() {
   }
 
   function breakBlock(block) {
+    //Clearar det ritade blocket, samt sätter blockinstansens storleksegenskaper
+    //till 0 utom för. De har då ingen storlek och kan därför inte bli träffade.
+    //x-egenskapen sätts också till 0, så att detectBlockSide inte ska kunna
+    //kollidera med blocket.
     ctx.clearRect(block.x - block.width - BLOCK_GAP - 1, block.y, block.width + 2, block.height);
-    console.log(block.x);
-    console.log(block.y);
-    console.log(block.width);
-    console.log(block.height);
     block.x = 0;
     block.width = 0;
     block.height = 0;
   }
 
+  //Känner av ifall bollen rör toppen eller botten av ett block. Den kollar bara
+  //ifall bollens topp rör blockets botten, eller ifall bollens botten rör
+  //blockets topp. Detta för att den bara kan kollidera på dessa 2 sätt.
   function detectBlockBottomTop (blockLayer) {
     if ((ballY > blockLayer[0].y && ballY < blockLayer[0].y + BLOCK_HEIGHT) || (ballY + BALL_SIZE > blockLayer[0].y && ballY + BALL_SIZE < blockLayer[0].y + BLOCK_HEIGHT)) {
       for (i = 0; i < blockLayer.length; i++) {
@@ -119,6 +114,10 @@ function main() {
     }
   }
 
+  //Funktion som känner av ifall bollen rör blockets sida. Kollar bara ifall
+  //bollens högersida rör blockets vänstersida och vice versa. Har en buffer på
+  //2 MOVEMENT_SPEED för att undvika buggen att bollen rör sig igenom
+  //hit-detectionen.
   function detectBlockSide(blockLayer) {
     if ((ballY > blockLayer[0].y && ballY < blockLayer[0].y + BLOCK_HEIGHT) || (ballY + BALL_SIZE > blockLayer[0].y && ballY + BALL_SIZE < blockLayer[0].y + BLOCK_HEIGHT)) {
       for (i = 0; i < blockLayer.length; i++) {
@@ -130,15 +129,18 @@ function main() {
     }
   }
 
+  //Funktion som hanterar all logik för hur bollen ska röra sig. Den körs
+  //repeterat för varje litet "steg" som bollen tar, med en kort delay för att
+  //inte overloada programmet.
   function ball() {
+    //Raderar den gamla instansen av bollen.
     ctx.clearRect(ballX, ballY, BALL_SIZE, BALL_SIZE);
 
+    //Uppdaterar bollens koordinater
     ballX += ballMoveX;
     ballY += ballMoveY;
 
-
-
-    //Hit-detection mot spelaren
+    //Hit-detection mot spelaren som gör att bollen studsar ifall den kolliderar
     if ((ballX + BALL_SIZE) > playerX && (ballX + BALL_SIZE) < (playerX + PLAYER_WIDTH) && (ballY + BALL_SIZE) > playerY && (ballY + BALL_SIZE) < (playerY + PLAYER_HEIGHT)) {
       //Kollar vilken sida av spelaren bollen räffar. Om det är vänstersidan går bollen vänster, annars höger.
       if (ballX + BALL_SIZE/2 > playerX && ballX + BALL_SIZE/2 < playerX + PLAYER_WIDTH/2) {
@@ -149,14 +151,17 @@ function main() {
       }
       ballMoveY *= -1;
     }
-    //Hit-detection för kanterna
+    //Hit-detection för kanterna av canvas-sidan. Om den rör kanten studsar
+    //bollen genom att ballMove variablen inverteras
     else if (ballX + BALL_SIZE > canvas.width || ballX < 0) {
       ballMoveX *= -1;
     }
     else if (ballY < 0) {
       ballMoveY *= -1;
     }
-    //Kör hit-detection för blocks undersida och ovansida
+    //Kör hit-detection för blocks undersida och ovansida. Om den returnar true
+    //så "studsar" bollen på ett passande sätt, genom att ballMove variablen
+    //inverteras (positivt tal blir negativt och vice versa)
     else if (detectBlockBottomTop(blockLayer1)) {
       ballMoveY *= -1;
     }
@@ -176,6 +181,7 @@ function main() {
       ballMoveX *= -1;
     }
 
+    //Ritar ut den nya bollen.
     ctx.fillStyle = BALL_COLOR;
     ctx.fillRect(ballX, ballY, BALL_SIZE, BALL_SIZE);
 
